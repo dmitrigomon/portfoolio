3974a7fbc9f58dc21c6a0e2a502d155a5f1df5b3
akadime <107892133+akadime@users.noreply.github.com>
2022-07-04 18:22:04 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <div> <img className="pea-pilt" src="https://modelagency.one/wp-content/uploads/2017/10/frankfurt-germany-model-agency-casting-fashion.jpg" alt="An image of a model" /></div>
+        <div>
+          <h2>Hello, I am Blaa Blaatens and this website is made for Programming or Design to show my portfolio</h2>
+        </div>
+      <div>
+        A horizontal line
+      </div>
+      <br/>
+
+      <div className="kast">
+        <img className="small-images" src="/XD-user design.png" alt="/XD-user design.png" />
+        <div>Work done related to <strong>USER DESIGN</strong></div>
+      </div>
+
+      <div className="kast">
+        <img className="small-images" src="/Hobbies-image.jfif" alt="/Hobbies-image.jfif" />
+        <div>Work done related to <strong>HOBBIES</strong></div>
+      </div>
+
+      <div className="kast">
+        <img className="small-images" src="/PS-image.jfif" alt="/PS-image.jfif" />
+        <div>Work done related to <strong>PHOTOSHOP</strong></div>
+      </div>
+            
     </div>
   );
 }
